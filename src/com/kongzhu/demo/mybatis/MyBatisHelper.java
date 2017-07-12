03832c486df29c8a284a0c670df0c7e75983b8ad
caicai <594665291@qq.com>
2017-07-12 08:53:26 +0800

    去掉main方法

--- src/com/kongzhu/demo/mybatis/MyBatisHelper.java
+++ src/com/kongzhu/demo/mybatis/MyBatisHelper.java
@@ -24,15 +24,4 @@ public class MyBatisHelper {
     public static SqlSession getSqlSession() {
         return sqlSessionFactory.openSession();
     }
-
-    public static void main(String[] args) {
-        SqlSession sqlSession = getSqlSession();
-        try {
-            Integer id = 1;
-            String message = sqlSession.selectOne("com.kongzhu.demo.mybatis.MessageMapper.selectById", id);
-            System.out.println(message);
-        } finally {
-            sqlSession.close();
-        }
-    }
 }
