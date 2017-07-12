4a090c5109989a3bfe0c0eb1a90b0b915d19f062
caicai <594665291@qq.com>
2017-07-12 13:26:51 +0800

    使用Java属性文件配置数据库连接信息

---
+++ src/com/kongzhu/demo/mybatis4/MyBatisHelper.java
@@ -0,0 +1,27 @@
+package com.kongzhu.demo.mybatis4;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisHelper {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        Reader reader = null;
+        try {
+            reader = new FileReader("src/com/kongzhu/demo/mybatis4/mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
