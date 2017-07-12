15bbe89e5af4bdbf930142e9ace0ac22e3f3d7c0
caicai <594665291@qq.com>
2017-07-12 10:02:31 +0800

    获取Mapper接口代码不需要try监控

--- src/com/kongzhu/demo/mybatis2/Application.java
+++ src/com/kongzhu/demo/mybatis2/Application.java
@@ -6,9 +6,9 @@ public class Application {
 
     public static void main(String[] args) {
         SqlSession sqlSession = MyBatisHelper.getSqlSession();
+        MessageMapper mapper = sqlSession.getMapper(MessageMapper.class);
         try {
             Integer id = 1;
-            MessageMapper mapper = sqlSession.getMapper(MessageMapper.class);
             String message = mapper.selectById(id);
             System.out.println(message);
         } finally {
