3a3aa0572d4c80bbc15eebf8f65821cdbb1ab523
caicai <594665291@qq.com>
2017-07-14 13:00:01 +0800

    添加注释

--- src/com/kongzhu/demo/mybatis2/Application.java
+++ src/com/kongzhu/demo/mybatis2/Application.java
@@ -5,13 +5,18 @@ import org.apache.ibatis.session.SqlSession;
 public class Application {
 
     public static void main(String[] args) {
+        // 从 SqlSessionFactory 中获取 SqlSession
         SqlSession sqlSession = MyBatisHelper.getSqlSession();
+        // 使用对于给定语句能够合理描述参数和返回值的接口
+        // 不但可以执行更清晰和类型安全的代码，而且还不用担心易错的字符串字面值以及强制类型转换
         MessageMapper mapper = sqlSession.getMapper(MessageMapper.class);
         try {
             Integer id = 1;
+            // 通过接口动态代理实现 SQL 语句检索单条记录
             String message = mapper.selectById(id);
             System.out.println(message);
         } finally {
+            // 始终记得关闭SqlSession
             sqlSession.close();
         }
     }
--- src/com/kongzhu/demo/mybatis4/MyBatisHelper.java
+++ src/com/kongzhu/demo/mybatis4/MyBatisHelper.java
@@ -1,27 +1,52 @@
 package com.kongzhu.demo.mybatis4;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
-
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+/**
+ * MyBatis工具类
+ * @author caiwenlong
+ *
+ */
 public class MyBatisHelper {
     private static SqlSessionFactory sqlSessionFactory;
 
+    /**
+     * 从 XML 中构建 SqlSessionFactory
+     */
     static {
-        Reader reader = null;
+        // XML配置文件相对路径
+        String resource = "com/kongzhu/demo/mybatis4/mybatis-config.xml";
+        InputStream in = null;
         try {
-            reader = new FileReader("src/com/kongzhu/demo/mybatis4/mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        } catch (FileNotFoundException e) {
+            // Resources工具类，使从 classpath 或其他位置加载资源文件更加容易
+            in = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {}
+            }
         }
     }
 
+    /**
+     * 从 SqlSessionFactory 中获取 SqlSession
+     * @return
+     */
     public static SqlSession getSqlSession() {
         return sqlSessionFactory.openSession();
     }
+
+    /**
+     * 该类不允许创建对象，仅作为工具类使用
+     */
+    private MyBatisHelper() {}
 }
